b0d073321638608cf0e7183370067bc25c0161dd
KelseyRocco <kelseyroccoh@gmail.com>
2021-06-16 12:57:04 -0400

    add bug model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Bug(models.Model):
+    name = models.CharField(max_length=100)
+    scientific = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    location = models.CharField(max_length=100)
+
